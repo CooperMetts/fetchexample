87a97058df632b8bbed57fe34b771e49c464b9ac
CooperMetts <coopermetts315@gmail.com>
2021-04-15 22:05:19 -0400

    All the changes

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+setup();
+
+function setup() {
+    getDog();
+  }
+
+  function getDog() {
+    fetch("https://random.dog/woof.json")
+      .then(response => response.json())
+      .then(data => updateDogImgSrc(data.url));
+  }
+
+  function updateDogImgSrc(src) {
+    let img = document.querySelector("img");
+    img.src = src;
+  }
